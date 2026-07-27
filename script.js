ff9829143a143eb9827b812c6ee6f99ddf63e9a5
Chaitra Yeccherla <108310906+PerplexingPenguin@users.noreply.github.com>
2026-07-27 19:45:25 -0400

    add random pokemon for card feature

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const cardInput = document.getElementById("card-input");
 const cardsDiv = document.getElementById("cards");
 const cardSubmit = document.getElementById("card-submit");
-const randomItems = [Bulbasaur, Ivysaur, Venusaur, Charmander, Charmeleon, Charizard, Squirtle, Wartortle, Blastoise, Caterpie, Metapod, Butterfree, Weedle, Kakuna, Beedrill, Pidgey, Pidgeotto, Pidgeo, Rattata, Raticate, Spearow, Fearow, Ekans, Arbok, Pikachu, Raichu, Sandshrew, Sandslash]
+const randomItems = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash"];
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+shuffle(randomItems);
 
 function createCards() {
-  cardsDiv.innerHTML = ''
+  cardsDiv.innerHTML = '';
   const numberOfCards = cardInput.value;
+  const randomItemsSliced = randomItems.slice(0, numberOfCards / 2);
+  randomItemsSliced.forEach((item) => {
+    randomItemsSliced.push(item);
+  });
+  shuffle(randomItemsSliced);
+
   for (let i = 0; i < numberOfCards; i++) {
     const card = document.createElement("DIV");
     card.classList.add("card");
-
     card.addEventListener("click", () => {
       if (card.classList.contains('flipped')) {
         card.classList.remove("flipped");
@@ -18,29 +35,12 @@ function createCards() {
       };
     });
 
-    card.innerHTML = "<div class='front'><img src='sylveonFocus-ezgif.com-crop.gif' width='100'/></div> <div class='back'><p></p></div>"
+    card.innerHTML = "<div class='front'><img src='sylveonFocus-ezgif.com-crop.gif' width='100'/></div> <div class='back'><p class='poke-identity'></p></div>"
+    const para = card.querySelector('.poke-identity')
+    para.innerHTML = randomItemsSliced[i];
     cardsDiv.appendChild(card);
   };
 };
-
-
-
-function fetchPokemonSprite(pokemon) {
-  let url = pokemon.url
-  fetch(url).then(response => response.json()).then(data => {
-    console.log(data.sprites.front_default);
-  })
-};
-
-function fetchPokemon() {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response => response.json()).then(function (allpokemon) {
-    allpokemon.results.forEach(function (pokemon) {
-      fetchPokemonSprite(pokemon);
-    })
-  })
-};
-
-fetchPokemon();
 
 cardSubmit.addEventListener("click", createCards);
 
