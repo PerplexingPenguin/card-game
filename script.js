13d0ee006020b716bbc6688478b62e970e644649
Chaitra Yeccherla <108310906+PerplexingPenguin@users.noreply.github.com>
2026-07-27 18:29:25 -0400

    add function to flip cards js

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const cardInput = document.getElementById("card-input");
 const cardsDiv = document.getElementById("cards");
 const cardSubmit = document.getElementById("card-submit");
 
-
 function createCards() {
   cardsDiv.innerHTML = ''
   const numberOfCards = cardInput.value;
   for (let i = 0; i < numberOfCards; i++) {
     const card = document.createElement("DIV");
     card.classList.add("card");
-    card.innerHTML = "<div class='front'><p></p></div> <div class='back'><img src='sylveonFocus-ezgif.com-crop.gif' width='100'/></div>"
+
+    card.addEventListener("click", () => {
+      card.classList.add("flipped");
+    });
+
+    card.innerHTML = "<div class='front'><img src='sylveonFocus-ezgif.com-crop.gif' width='100'/></div> <div class='back'></div>"
     cardsDiv.appendChild(card);
   };
 };
